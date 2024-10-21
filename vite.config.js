0f3c28bf50cde589aaf45ff4a864b10ae13c3de7
rkeerthient <rkeerthi@yext.com>
2024-10-21 16:53:33 +0530

    added rollup

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from "@vitejs/plugin-react";
 import yextSSG from "@yext/pages/vite-plugin";
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      external: ["ssh2-sftp-client"],
+    },
+  },
   plugins: [react(), yextSSG()],
 });
