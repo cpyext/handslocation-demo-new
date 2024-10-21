c52672a32811bbb59a7438bb34e3cbf565021566
cpyext <cpurighalla@yext.com>
2024-10-21 13:29:30 +0530

    Minor CSS

--- src/templates/location.tsx
+++ src/templates/location.tsx
@@ -117,7 +117,7 @@ const Location: Template<TemplateRenderProps> = ({
 
   return (
     <>
-      <div className="bg-[#043b76] text-white py-3">
+      <div className="bg-[#043b76] text-white py-1">
         <div className="container mx-auto flex justify-end">
           <a href="#" className="text-white text-lg px-4 py-2 rounded bg-[#043b76] border-2 border-[#043b76] hover:bg-[#FFFFFF] hover:text-[#043b76] hover:border-[#043b76] text-center">
             Account
@@ -170,8 +170,8 @@ const Location: Template<TemplateRenderProps> = ({
             {/* Right Section - Location Details */}
             <div>
               <h1 className="text-3xl font-bold text-gray-800 mb-4">Hand & Stone - Chesterfield, VA</h1>
-              <p className="text-lg text-gray-600 mb-2">Call us: <span className="font-semibold text-blue-700">(804) 639-1113</span></p>
-              <p className="text-lg text-gray-600 mb-4">Find us: <span className="font-semibold text-blue-700">7204 Hancock Village Drive, Chesterfield, VA 23832</span></p>
+              <p className="text-lg text-gray-600 mb-2">Call us: <span className="font-semibold text-[#043b76]">(804) 639-1113</span></p>
+              <p className="text-lg text-gray-600 mb-4">Find us: <span className="font-semibold text-[#043b76]">7204 Hancock Village Drive, Chesterfield, VA 23832</span></p>
 
               <div className="flex flex-col space-y-4 mb-4">
                 <a
