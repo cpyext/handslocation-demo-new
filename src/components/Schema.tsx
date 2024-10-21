8b91bd905b7d3693937485fbf97838ca58c10cf9
rkeerthient <rkeerthi@yext.com>
2024-10-21 19:00:17 +0530

    added offers

--- src/components/Schema.tsx
+++ src/components/Schema.tsx
@@ -7,62 +7,29 @@ const Schema = (props: any) => {
   const telephone = document.mainPhone;
   const description = document.decription;
   const faqsList: any = [];
-  const productsList: any = [];
-  const itemListElement: any = [];
   const servicesOffered: any = [];
+  const offersOffered: any = [];
   if (document.c_relatedServices) {
-    document.c_relatedServices.forEach((item: any) => {
-      servicesOffered.push({
-        "@type": "OfferCatalog",
-        itemOffered: {
-          "@type": "Service",
-          name: `${item.name}`,
-        },
-      });
-    });
-  }
-  if (document.c_relatedOffers) {
-    document.c_relatedOffers.forEach((item: any) => {
-      itemListElement.push({
+    document.c_relatedServices.map((item) =>
+      offersOffered.push({
         "@type": "Offer",
         itemOffered: {
           "@type": "Service",
           name: `${item.name}`,
         },
-      });
-    });
+      })
+    );
   }
-
-  if (document.c_entityCollection) {
-    document.c_entityCollection.forEach((item1: any, index: any) => {
-      item1.c_products.forEach((item: Product, index: any) => {
-        console.log(JSON.stringify(item));
-
-        productsList.push({
-          "@type": "ListItem",
-          position: parseInt(index) + 1,
-          item: {
-            "@type": "Product",
-            name: item.name,
-            image: item.photoGallery && item.photoGallery[0].image.url,
-            category: item.c_category && item.c_category,
-            sku: document.id,
-            aggregateRating: {
-              "@type": "AggregateRating",
-              bestRating: "5",
-              ratingCount: item.c_reviews,
-              ratingValue: item.c_rating,
-            },
-            offers: {
-              "@type": "Offer",
-              availability: "https://schema.org/InStock",
-              price: item.price && item.price.value,
-              priceCurrency: item.price && item.price.currencyCode,
-            },
-          },
-        });
-      });
-    });
+  if (document.c_relatedOffers) {
+    document.c_relatedOffers.map((item) =>
+      servicesOffered.push({
+        "@type": "Offer",
+        itemOffered: {
+          "@type": "Service",
+          name: `${item.name}`,
+        },
+      })
+    );
   }
 
   if (document.c_relatedFAQs) {
@@ -103,31 +70,13 @@ const Schema = (props: any) => {
             name: "Store services",
             itemListElement: servicesOffered,
           },
-          offers: [
-            {
-              "@type": "Offer",
-              name: "Intro Image Classic Facial",
-              price: "79.95",
-              priceCurrency: "USD",
-              url: "https://handandstone.com/book-an-appointment/?location_id=0c4b087e-3de1-4412-b05f-bee1c335f9e8",
-              description: `Introductory pricing for first-time customers only. 
-                Service includes time for dressing and consultation.`,
-            },
-            {
-              "@type": "Offer",
-              name: "Introductory Swedish Massage 1 Hour",
-              price: "79.95",
-              priceCurrency: "USD",
-              url: "https://handandstone.com/book-an-appointment/?location_id=0c4b087e-3de1-4412-b05f-bee1c335f9e8",
-            },
-          ],
         }}
       />
       <JsonLd<ItemList>
         item={{
           "@context": "https://schema.org",
           "@type": "ItemList",
-          itemListElement: productsList,
+          itemListElement: offersOffered,
         }}
       />
       <JsonLd<FAQPage>
